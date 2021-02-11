f8fe0ed59b3e8cfff22dea1d69013fd20edd927b
Jason Nordheim <jason.nordheim@gmail.com>
2021-02-11 15:05:53 -0700

    added ChatEngine component inside App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,15 @@
 import React from "react";
-import { } from 'react'
+import { ChatEngine } from "react-chat-engine";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="1ddc2650-e4a3-49b2-a059-2d9efac5bfb2"
+      userName="helloWorld" // admin user
+      userSecret="helloWorld" /// admin password
+    />
+  );
 };
 
 export default App;
