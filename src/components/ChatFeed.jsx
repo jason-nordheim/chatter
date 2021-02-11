e0c8692397bd006203710af1fc13959f8875b2a4
Jason Nordheim <jason.nordheim@gmail.com>
2021-02-11 16:37:54 -0700

    misc. formatting changes

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -4,48 +4,53 @@ import MyMessage from "./MyMessage";
 import TheirMessage from "./TheirMessage";
 
 const ChatFeed = (props) => {
-  const { chats, activeChat, userName, messages } = props 
+    const { chats, activeChat, userName, messages } = props;
 
-  const chat = chats && chats[activeChat]
+    const chat = chats && chats[activeChat];
 
     const renderMessages = () => {
-        const keys = Object.keys(messages) 
+        const keys = Object.keys(messages);
         return keys.map((key, index) => {
-            const message = messages[key]
-            const lastMessageKey = index === 0 ? null : keys[index - 1]  
-            const isMyMessage = userName === message.sender.username; 
+            const message = messages[key];
+            const lastMessageKey = index === 0 ? null : keys[index - 1];
+            const isMyMessage = userName === message.sender.username;
             return (
-                <div key={`msg_${index}`} style={{ width: '100%'}}>
+                <div key={`msg_${index}`} style={{ width: '100%' }}>
                     <div className="message-block">
                         {
-                            isMyMessage 
-                            ? <MyMessage message={message} /> 
-                            : <TheirMessage message={message} lastMessage={messages[lastMessageKey]} /> 
+                            isMyMessage
+                                ? <MyMessage message={message} />
+                                : <TheirMessage message={message} lastMessage={messages[lastMessageKey]} />
                         }
                     </div>
-                    <dev className="read-reciepts" style={{ marginRight: isMyMessage ?'18px': '0px', marginLeft: isMyMessage ?'0': '68px'}}>
-                        read-reciepts 
-                    </dev>
+                    <div
+                        className="read-reciepts"
+                        style={{
+                            marginRight: isMyMessage ? '18px' : '0px',
+                            marginLeft: isMyMessage ? '0' : '68px'
+                        }}>
+                        read-reciepts
+                    </div>
                 </div>
-            )
-        })
-    }
+            );
+        });
+    };
 
-    if (!chat) return 'loading..'    
+    if (!chat) return 'loading..';
 
-  return <div className="chat-feed">
-      <div className="chat-title-container">
-          <div className="chat-title">{ chat?.title} </div>
-          <div className="chat-subtitle">
-              {chat.people.map((person) => ` ${person.person.username }`)}
-          </div>
-      </div>
-      { renderMessages()}
-      <div style={{ height: '100px'}}></div>
-      <div className="message-form-container">
-          <MessageForm {...props} /> 
-      </div>
-  </div>;
+    return <div className="chat-feed">
+        <div className="chat-title-container">
+            <div className="chat-title">{chat?.title} </div>
+            <div className="chat-subtitle">
+                {chat.people.map((person) => ` ${person.person.username}`)}
+            </div>
+        </div>
+        {renderMessages()}
+        <div style={{ height: '100px' }}></div>
+        <div className="message-form-container">
+            <MessageForm {...props} />
+        </div>
+    </div>;
 };
 
 export default ChatFeed;
