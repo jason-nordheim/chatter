cf8e9d19cc891a278d5e56099dad22f461f55ef0
Jason Nordheim <jason.nordheim@gmail.com>
2021-02-11 15:10:33 -0700

    passed props to skeleton Chat feed

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { ChatEngine } from "react-chat-engine";
 import "./App.css";
+import ChatFeed from "./ChatFeed";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       projectID="1ddc2650-e4a3-49b2-a059-2d9efac5bfb2"
       userName="helloWorld" // admin user
       userSecret="helloWorld" /// admin password
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
     />
   );
 };
