6eaed6e4cbe10fcc1db432d946fbf06b9d97fe17
Jason Nordheim <jason.nordheim@gmail.com>
2021-02-11 16:38:06 -0700

    created message form, implemented sending a message

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { sendMessage, isTyping } from 'react-chat-engine';
+import { act } from "react-dom/test-utils";
 
-export const MessageForm = () => {
+export const MessageForm = (props) => {
+    const { activeChat } = props;
+    const [value, setValue] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const text = value.trim();
+        if (text.length > 0) {
+            sendMessage(props, activeChat, { text });
+            setValue('');
+        }
+    };
+
+    const handleChange = (e) => {
+        setValue(e.target.value);
+        isTyping({ ...props, activeChat }, activeChat);
+    };
+
     return (
-        <div>
-            Message Form
-        </div>
-    )
-}
+        <form
+            className="message-form"
+            onSubmit={handleSubmit}
+        >
+            <input
+                className="message-input"
+                placeholder="Send a message..."
+                value={value}
+                onChange={handleChange}
+                onSubmit={handleSubmit}
+            />
+            <label htmlFor="upload-button">
+                <span className="image-button"></span>
+            </label>
+        </form>
+    );
+};
