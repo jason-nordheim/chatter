323d5a559a81968ed243d3383ad7ae9005caffc5
Jason Nordheim <jason.nordheim@gmail.com>
2021-02-11 15:51:45 -0700

    added jsx for TheirMessage component

--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from 'react';
 
-export const TheirMessage = () => {
+export const TheirMessage = ({ message, lastMessage }) => {
+    const isFirstMessageByUser = !lastMessage || lastMessage.sender.username !== message.sender.username;
     return (
-        <div>
-            Their Message 
-        </div>
-    )
-} 
+        <div className="message-row">
+            { isFirstMessageByUser && (
+                <div className="message-avatar"
+                    style={{ backgroundImage: `url(${message?.sender?.avatar})` }}
+                />
+            )}
+            {
+                message?.attachements?.length > 0
+                    ? (
+                        <img src={message.attachements[0].file}
+                            alt="message-attachment"
+                            className="message-image"
+                            style={{ marginLeft: isFirstMessageByUser ? '4px' : '48px' }} />
+                    ) : (
+                        <div className="message"
+                            style={{
+                                float: 'left',
+                                backgroundColor: '#CABCDC',
+                                marginLeft: isFirstMessageByUser ? '4px' : '48px'
+                            }} >
+                            { message.text}
+                        </div>
+                    )
+            }
+    }
+        </div >
+    );
+};
 
-export default TheirMessage
+export default TheirMessage;
