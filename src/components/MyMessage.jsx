070bd05a0084724e7ed0c834d1ee560673960ee9
Jason Nordheim <jason.nordheim@gmail.com>
2021-02-11 15:43:38 -0700

    added actual JSX for MyMessage

--- src/components/MyMessage.jsx
+++ src/components/MyMessage.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from 'react';
 
-const MyMessage = () => {
+const MyMessage = ({ message }) => {
+    if (message?.attachements?.length > 0) {
+        // message is an image
+        return (
+            <img src={message.attachements[0].file}
+                alt="message-attachment"
+                className="message-image"
+                style={{ float: 'right' }} />
+        );
+    }
     return (
-        <div>
-            My Message
+        <div className="message"
+            style={{
+                float: 'right',
+                marginRight: '18px',
+                color: 'white',
+                backgroundColor: '#3B2A50'
+            }} >
+            { message.text}
         </div>
-    )
-}
+    );
+};
 
-export default MyMessage
+export default MyMessage;
