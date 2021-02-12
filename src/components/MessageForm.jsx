342aebc0b211991550098b438ea844d060bb332d
Jason Nordheim <jason.nordheim@gmail.com>
2021-02-12 09:57:29 -0700

    finished message form

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,23 +1,47 @@
 import React, { useState } from 'react';
 import { sendMessage, isTyping } from 'react-chat-engine';
-import { act } from "react-dom/test-utils";
+import { SendOutlined, PictureOutlined } from '@ant-design/icons';
+
+// import { isTyping } from 'react-chat-engine';
+
+// const authObject = {'Project-ID': '0000-00-00-0000', 'User-Name': 'adam', 'User-Secret': 'pass1234'}
+// const chatID = 1
+// const username = 'adam'
+
+// isTyping(authObject, chatID, username)
 
 export const MessageForm = (props) => {
-    const { activeChat } = props;
+    const { activeChat, creds, chatId } = props;
     const [value, setValue] = useState('');
+    const authObject = { 'Project-ID': '', 'User-Name': '', 'User-Secret': '' };
+
+    console.log(creds, chatId);
+
 
     const handleSubmit = (e) => {
         e.preventDefault();
         const text = value.trim();
         if (text.length > 0) {
-            sendMessage(props, activeChat, { text });
+            sendMessage(creds, chatId, { text });
             setValue('');
         }
     };
 
     const handleChange = (e) => {
         setValue(e.target.value);
-        isTyping({ ...props, activeChat }, activeChat);
+        let sendTyping = true;
+        if (sendTyping) {
+            isTyping({ ...props, activeChat }, activeChat);
+            sendTyping = false;
+            setTimeout(() => {
+                sendTyping = true;
+            }, 250);
+        }
+
+    };
+
+    const handleUpload = (e) => {
+        sendMessage(creds, chatId, { files: e.target.files, text: '' });
     };
 
     return (
@@ -33,8 +57,20 @@ export const MessageForm = (props) => {
                 onSubmit={handleSubmit}
             />
             <label htmlFor="upload-button">
-                <span className="image-button"></span>
+                <span className="image-button">
+                    <PictureOutlined className="picture-icon" />
+                </span>
             </label>
+            <input
+                id="upload-button"
+                type="file"
+                multiple={false}
+                style={{ display: 'none' }}
+                onChange={handleUpload}
+            />
+            <button type="submit" className="send-button">
+                <SendOutlined className="send-icon" />
+            </button>
         </form>
     );
-};
+};;
