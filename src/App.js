a57cad0e59bd54b2befd6de9482e7b96f2c1180d
Jason Nordheim <jason.nordheim@gmail.com>
2021-02-12 11:16:06 -0700

    added logic to show login form if no authenticated user is found

--- src/App.js
+++ src/App.js
@@ -2,17 +2,27 @@ import React from "react";
 import { ChatEngine } from "react-chat-engine";
 import "./App.css";
 import ChatFeed from "./components/ChatFeed";
+import LoginForm from "./components/LoginForm";
+import {
+  projectID,
+  isAuthenticated,
+  authenticatedUser,
+} from "./util/ChatEngineUtils";
 
 const App = () => {
-  return (
-    <ChatEngine
-      height="100vh"
-      projectID="1ddc2650-e4a3-49b2-a059-2d9efac5bfb2"
-      userName="fooBar" // admin user
-      userSecret="fooBar" /// admin password
-      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
-    />
-  );
+  if (!isAuthenticated()) return <LoginForm />;
+  else {
+    const { username, password } = authenticatedUser();
+    return (
+      <ChatEngine
+        height="100vh"
+        projectID={projectID}
+        userName={username} // admin user
+        userSecret={password} /// admin password
+        renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      />
+    );
+  }
 };
 
 export default App;
