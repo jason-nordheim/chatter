9b8c603f0c671ffc5792bec9b8df357e2bef2cae
Jason Nordheim <jason.nordheim@gmail.com>
2021-02-12 11:15:45 -0700

    created login form with associated utility functions

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+
+import axios from 'axios';
+import { authObject, projectID, setAuthenticated } from "../util/ChatEngineUtils";
+
+export const LoginForm = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPasswod] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const authObj = authObject(username, password);
+
+        try {
+            // send username & password to ChatEngine
+            const options = { headers: authObj };
+            const res = await axios.get('https://api.chatengine.io/chats', options);
+            // (SUCCESS) = Returns messages for user => login 
+            setAuthenticated(username, password);
+            window.location.reload();
+        } catch (error) {
+            // (UNSUCCESSFUL) = No Messages returned => error: try again 
+
+        }
+
+    };
+
+    return (
+        <div className="wrapper">
+            <div className="form">
+                <h1 className="title">Chatter</h1>
+                <form onSubmit={handleSubmit}>
+                    <input type="text"
+                        className="input"
+                        placeholder="Username"
+                        value={username}
+                        onChange={e => setUsername(e.target.value)}
+                        required
+                    />
+                    <input type="password"
+                        className="input"
+                        placeholder="Password"
+                        value={password}
+                        onChange={e => setPasswod(e.target.value)}
+                        required
+                    />
+                    <div align="center">
+                        <button type="submit" className="button">
+                            <span>Login</span>
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default LoginForm;
--- src/util/ChatEngineUtils.js
+++ src/util/ChatEngineUtils.js
@@ -4,3 +4,31 @@ export const isImage = (message) =>
   message.attachments?.length > 0 &&
   message.attachments[0] &&
   message.attachments[0].file;
+
+export const projectID = "1ddc2650-e4a3-49b2-a059-2d9efac5bfb2";
+
+export const authObject = (username, password, projectId = projectID) => {
+  return {
+    "Project-ID": projectID,
+    "User-Name": username,
+    "User-Secret": password,
+  };
+};
+
+export const setAuthenticated = (username, password) => {
+  localStorage.setItem("username", username);
+  localStorage.setItem("password", password);
+};
+
+export const isAuthenticated = () => {
+  if (!localStorage.getItem("username")) return false;
+  else if (!localStorage.getItem("password")) return false;
+  else return true;
+};
+
+export const authenticatedUser = () => {
+  return {
+    username: localStorage.getItem("username"),
+    password: localStorage.getItem("password"),
+  };
+};
